38544380f59119d56d104989992c7bb4d41c8eca
Gastón Martínez Castro <38297466+Gastonm123@users.noreply.github.com>
2024-08-28 01:54:10 +0000

    Clase 27/8

--- clase26/bloc-notas/src/App.js
+++ clase26/bloc-notas/src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import Boton from './Boton'
+import React from 'react';
+import Nota from './Nota';
 
 export default function App() {
   return (
-    <form>
-      <label for="nombre">Nombre</label> <br/>
-      <input type="text" name="nombre"/> <br/>
-      <label for="apellido">Apellido</label> <br/>
-      <input type="text" name="apellido"/> <br/>
-      <label for="fecha_nacimiento">Fecha Nacimiento</label> <br/>
-      <input type="date" name="fecha_nacimiento"/> <br/> <br/>
-      <Boton text="Confirmar"/>
-    </form>
+    <table>
+      <tr>
+        <Nota />
+        <Nota />
+        <Nota />
+      </tr>
+      <tr>
+        <Nota />
+        <Nota />
+        <Nota />  
+      </tr>
+    </table>
   );
 }
--- clase26/bloc-notas/src/Boton.js
+++ clase26/bloc-notas/src/Boton.js
@@ -1,10 +1,15 @@
 // Sintaxis import y export
 import React from 'react'
 import TextoBoton from './TextoBoton'
+import './Boton.css'
+
+function ManejadorClick() {
+    alert('se hizo click')
+}
 
 export default function Boton({ text }) {
     return (
-        <button>
+        <button className="enviar" onClick={ManejadorClick}>
             <TextoBoton text={text}/>
         </button>
     )
